2def68e3ab52c249df135d4bce1d07f61af25225
findolor <anakisci@gmail.com>
2022-11-10 12:59:05 +0300

    Add test pages to fee

--- pages/modules/fee/create.tsx
+++ pages/modules/fee/create.tsx
@@ -6,6 +6,7 @@ import { TextInput } from "../../../components/TextInput"
 import { useOfflineSigners, useAccount } from "graz"
 import { getSigningClient } from "../../../utils/getSigningClient"
 import { JsonViewer } from "../../../components/JsonViewer"
+import { ContractForm } from "../../../components/ContractForm"
 
 export default function FeeModuleCreate() {
   const { data: account } = useAccount()
@@ -41,31 +42,25 @@ export default function FeeModuleCreate() {
         description="Fee module is used for defining dadhsahdas dasj dajajh whajdwhjdw dajdahd dwadad."
         documentation="https://docs.komple.io/komple/framework-fundamentals/modules/fee-module"
       />
-      <div className="mt-20 flex">
-        <ContractTabs contract="Fee" isModule />
-        <div className="w-20" />
-        <div>
-          <TextInput
-            title="Code ID"
-            subtitle="Code ID of the contract"
-            placeholder="123"
-            onChange={setCodeId}
-          />
-          <TextInput
-            title="Admin"
-            subtitle="Address of the contract admin"
-            placeholder="juno..."
-            onChange={setAdmin}
-          />
-          <Button
-            text="Create Fee Module"
-            onClick={instantiate}
-            disabled={disabled}
-          />
-        </div>
-        <div className="w-20" />
-        {response && <JsonViewer json={response} />}
-      </div>
+      <ContractForm name="Fee" isModule={true} response={response}>
+        <TextInput
+          title="Code ID"
+          subtitle="Code ID of the contract"
+          placeholder="123"
+          onChange={setCodeId}
+        />
+        <TextInput
+          title="Admin"
+          subtitle="Address of the contract admin"
+          placeholder="juno..."
+          onChange={setAdmin}
+        />
+        <Button
+          text="Create Fee Module"
+          onClick={instantiate}
+          disabled={disabled}
+        />
+      </ContractForm>
     </div>
   )
 }
--- pages/modules/fee/execute.tsx
+++ pages/modules/fee/execute.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react"
+import { Button } from "../../../components/Button"
+import { ContractForm } from "../../../components/ContractForm"
 import { ContractHeader } from "../../../components/ContractHeader"
-import { ContractTabs } from "../../../components/ContractTabs"
+import { TextInput } from "../../../components/TextInput"
 
-export default function FeeModuleCreate() {
+export default function FeeModuleExecute() {
+  const [feeType, setFeeType] = useState("")
+  const [moduleName, setModuleName] = useState("")
+  const [feeName, setFeeName] = useState("")
+  const [data, setData] = useState({})
+  const [response, setResponse] = useState({})
+
+  const execute = async () => {}
+
+  const disabled = feeType === "" || moduleName === "" || feeName === ""
+
   return (
     <div className="h-full w-full">
       <ContractHeader
@@ -9,10 +22,36 @@ export default function FeeModuleCreate() {
         description="Fee module is used for defining dadhsahdas dasj dajajh whajdwhjdw dajdahd dwadad."
         documentation="https://docs.komple.io/komple/framework-fundamentals/modules/fee-module"
       />
-      <div className="mt-20 flex">
-        <ContractTabs contract="Fee" isModule />
-        <div className="w-10" />
-      </div>
+      <ContractForm name="Fee" isModule={true} response={response}>
+        <TextInput
+          title="Fee Type"
+          subtitle="dasdsa"
+          placeholder="juno..."
+          onChange={setFeeType}
+        />
+        <TextInput
+          title="Module Name"
+          subtitle="Name of the module that will be used"
+          placeholder="mint"
+          onChange={setModuleName}
+        />
+        <TextInput
+          title="Fee Name"
+          subtitle="Name of the fee that will be used as identifier"
+          placeholder="price"
+          onChange={setFeeName}
+        />
+        <TextInput
+          title="Data"
+          subtitle="Data that will determine the fee value and payment address"
+          onChange={setData}
+        />
+        <Button
+          text="Execute Fee Module"
+          onClick={execute}
+          disabled={disabled}
+        />
+      </ContractForm>
     </div>
   )
 }
