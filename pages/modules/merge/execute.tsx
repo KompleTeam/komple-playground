294041d5c2e79a0addc30eb7cb40c4b47ea90d84
findolor <anakisci@gmail.com>
2022-12-02 17:15:32 +0300

    Implement merge module

--- pages/modules/merge/execute.tsx
+++ pages/modules/merge/execute.tsx
@@ -7,7 +7,7 @@ import { KompleClient } from "komplejs"
 import Head from "next/head"
 import { useWallet } from "@cosmos-kit/react"
 import { toBinary } from "@cosmjs/cosmwasm-stargate"
-import { useMergeModuleStore } from "store"
+import { useAppStore, useMergeModuleStore } from "store"
 import {
   MergeModuleMerge,
   MergeModulePermissionMerge,
@@ -27,6 +27,7 @@ export default function MergeModuleExecute() {
   const { getSigningCosmWasmClient, offlineSigner } = useWallet()
 
   const store = useMergeModuleStore((state) => state)
+  const setLoading = useAppStore((state) => state.setLoading)
 
   const [executeMsg, setExecuteMsg] = useState<string>("")
   const [response, setResponse] = useState<any>({})
@@ -38,6 +39,8 @@ export default function MergeModuleExecute() {
 
   const submit = async ({ contract }: { contract: string }) => {
     try {
+      setLoading(true)
+
       const signingClient = await getSigningCosmWasmClient()
       if (signingClient === undefined || offlineSigner === undefined) {
         throw new Error("client or signer is not ready")
@@ -92,9 +95,11 @@ export default function MergeModuleExecute() {
           return setResponse(await executeClient.lockExecute())
         }
       }
+
+      setLoading(false)
     } catch (error: any) {
-      console.log(error)
       setResponse(error.message)
+      setLoading(false)
     }
   }
 
