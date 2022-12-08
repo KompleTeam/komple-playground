2fa155602c777aed228eac27c7509c6e96ff5e96
findolor <anakisci@gmail.com>
2022-12-08 12:54:28 +0300

    Add animation to section component

--- components/SectionBox/index.tsx
+++ components/SectionBox/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { motion } from "framer-motion"
 
 export const SectionBox = ({
   title,
@@ -10,13 +11,23 @@ export const SectionBox = ({
   link: string
 }) => {
   return (
-    <Link href={link}>
-      <div className="h-[120px] w-[270px] p-4 border-[1px] border-komple-black-200 rounded-lg mx-4 hover:border-white/30 hover:bg-white/5">
-        <div className="text-[24px] font-semibold text-white">{title}</div>
-        <div className="text-komple-black-100 leading-5 mt-1">
-          {description}
+    <motion.div
+      initial={{ opacity: 0, scale: 0.7 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.6,
+        delay: 0.1,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+    >
+      <Link href={link}>
+        <div className="h-[120px] w-[270px] p-4 border-[1px] border-komple-black-200 rounded-lg mx-4 hover:border-white/30 hover:bg-white/5">
+          <div className="text-[24px] font-semibold text-white">{title}</div>
+          <div className="text-komple-black-100 leading-5 mt-1">
+            {description}
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </motion.div>
   )
 }
