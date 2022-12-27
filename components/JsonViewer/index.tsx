424054c42ad3e97af6310ab93cc9dba9de581478
findolor <anakisci@gmail.com>
2022-12-27 17:09:37 +0300

    Add copy button to json viewer

--- components/JsonViewer/index.tsx
+++ components/JsonViewer/index.tsx
@@ -13,6 +13,11 @@ export const JsonViewer = ({
 }) => {
   const [open, setOpen] = useState(isOpen)
 
+  const copyOnClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation()
+    navigator.clipboard.writeText(JSON.stringify(json))
+  }
+
   return (
     <div className="w-[400px] max-w-[400px] overflow-hidden">
       <div className="border-[2px] border-komple-black-300 rounded-md">
@@ -24,14 +29,25 @@ export const JsonViewer = ({
           )}
           onClick={() => setOpen(!open)}
         >
-          <div>{title}</div>
-          <Image
-            src="/icons/arrow.svg"
-            width={14}
-            height={14}
-            alt="Arrow Icon"
-            className={clsx(open && "transform rotate-180")}
-          />
+          <div className="flex items-center">
+            <div className="mr-2">{title}</div>
+            <Image
+              src="/icons/arrow.svg"
+              width={14}
+              height={14}
+              alt="Arrow Icon"
+              className={clsx(open && "transform rotate-180")}
+            />
+          </div>
+          <button onClick={copyOnClick}>
+            <Image
+              src="/icons/copy.svg"
+              width={12}
+              height={12}
+              alt="Arrow Icon"
+              className="mr-2"
+            />
+          </button>
         </button>
         {open && (
           <pre className="text-white text-[14px] p-3 overflow-scroll">
